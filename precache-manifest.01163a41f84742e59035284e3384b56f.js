f702b99103ae315e5eeea0b657db9c94ecdd89ae
ervinmust <mustervin22@gmail.com>
2020-08-14 09:11:40 +0300

    Add files via upload

---
+++ precache-manifest.01163a41f84742e59035284e3384b56f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93e42bdc60e903a24026415c295e338c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b65c3a7ddd8983b436e3",
+    "url": "/static/css/main.9e034465.chunk.css"
+  },
+  {
+    "revision": "ab12b72ec6cd5010e57a",
+    "url": "/static/js/2.42df7b18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.42df7b18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b65c3a7ddd8983b436e3",
+    "url": "/static/js/main.e4693d89.chunk.js"
+  },
+  {
+    "revision": "c0ccae055cb2c07e921a",
+    "url": "/static/js/runtime-main.4483ebb2.js"
+  }
+]);
